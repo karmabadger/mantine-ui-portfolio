9d54f6124aa6212a060f5c5147d467d167afb3a9
karmabadger <karmabadger@outlook.com>
2023-05-13 02:49:23 -0400

    added a SocialIcons component to put all the social icons together

---
+++ components/social-icons/SocialIcons.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { SocialIcon } from "react-social-icons";
+import React from "react";
+import { Flex } from "@mantine/core";
+
+const margin = 2;
+
+const SocialIcons = () => {
+  return (
+    <>
+      <Flex>
+        <SocialIcon
+          url="https://www.linkedin.com/in/wen-chen-1a1a1a1a1/"
+          id="linkedin"
+          style={{ height: 35, width: 35, margin }}
+        />
+        <SocialIcon
+          id="github"
+          url="https://github.com/karmabadger"
+          style={{ height: 35, width: 35, margin }}
+        />
+        <SocialIcon
+          id="email"
+          url="mailto:wenxuan27@outlook.com"
+          style={{ height: 35, width: 35, margin }}
+        />
+      </Flex>
+    </>
+  );
+};
+
+export default SocialIcons;
