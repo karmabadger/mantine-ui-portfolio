af5806021bc6d31db4577342e61e9eb1095835be
karmabadger <83666608+karmabadger@users.noreply.github.com>
2024-01-26 21:30:46 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,9 +65,9 @@ export default function Home() {
           </Box>
           <Box>
             <Text>
-              I'm Wen, a passionate Software Engineer with over 7 years of experience in crafting exceptional digital experiences. Armed with a Computer Science degree from McGill University, I specialize in designing and implementing innovative full-stack web applications and blockchain solutions.
-But what truly sets my heart on fire is game development; it's where I transform my personal passion into captivating virtual worlds.
-Beyond the world of technology, I'm a curious explorer. I delve into history, immerse myself in the intricacies of politics, and navigate the complexities of finance. When I'm not busy coding, you'll find me on the tennis court or badminton court, or perhaps creating harmonious melodies through music.
+              I&apos;m Wen, a passionate Software Engineer with over 7 years of experience in crafting exceptional digital experiences. Armed with a Computer Science degree from McGill University, I specialize in designing and implementing innovative full-stack web applications and blockchain solutions.
+But what truly sets my heart on fire is game development; it&apos;s where I transform my personal passion into captivating virtual worlds.
+Beyond the world of technology, I&apos;m a curious explorer. I delve into history, immerse myself in the intricacies of politics, and navigate the complexities of finance. When I&apos;m not busy coding, you&apos;ll find me on the tennis court or badminton court, or perhaps creating harmonious melodies through music.
 Join me on this exciting journey of innovation, creativity, and endless possibilities.
             </Text>
             <br />
